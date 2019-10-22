c0768609f7c3ef4aafc646db6c286cce5b16d546
Megan <megan.miller@smoldev.dev>
2019-10-22 10:31:45 -0500

    Adding posts to the redux store, added delete and update action creators, added GSAP

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,3 +16,54 @@ export const SEARCH_FAILED = "SEARCH_FAILED";
 export const search = item => dispatch => {
 	// code...
 };
+
+// Post actions
+export const POST_START = "POST_START";
+export const POST_SUCCESS = "POST_SUCCESS";
+export const POST_FAILED = "POST_FAILED";
+
+export const addPost = post => dispatch => {
+	dispatch({ type: POST_START });
+	axiosWithAuth()
+		.post("/createPost", post)
+		.then(res => {
+			dispatch({ type: POST_SUCCESS, payload: res.data });
+		})
+		.catch(err => {
+			dispatch({ type: POST_FAILED, payload: err.response });
+		});
+};
+
+// updating post action
+export const UPDATING_START = "UPDATING_START";
+export const UPDATE_SUCCESS = "UPDATE_SUCCESS";
+export const UPDATE_FAILED = "UPDATE_FAILED";
+
+export const updatePost = (id, changes) => dispatch => {
+	dispatch({ type: UPDATING_START });
+	axiosWithAuth()
+		.put(`${id}`, changes)
+		.then(res => {
+			dispatch({ type: UPDATE_SUCCESS, payload: changes });
+		})
+		.catch(err => {
+			dispatch({ type: UPDATE_FAILED, payload: err.response });
+		});
+};
+
+// deleting action
+export const DELETE_START = "DELETE_START";
+export const DELETE_SUCCESS = "DELETE_SUCCESS";
+export const DELETE_FAILED = "DELETE_FAILED";
+
+export const deletePost = id => dispatch => {
+	dispatch({ type: DELETE_START });
+	axiosWithAuth()
+		.delete(`${id}`)
+		.then(res => {
+			dispatch({ type: DELETE_SUCCESS, payload: id });
+		})
+		.catch(err => {
+			dispatch({ type: DELETE_FAILED, payload: err.response });
+		});
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { POST_START, POST_SUCCESS, POST_FAILED } from "../actions";
+
 const initialState = {
 	posts: [],
 	addingPost: false,
@@ -10,5 +12,21 @@ export const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		default:
 			return state;
+		case POST_START:
+			return {
+				...state,
+				addingPost: true,
+			};
+		case POST_SUCCESS:
+			return {
+				...state,
+				posts: [...state.posts, action.payload],
+				addingPost: false,
+			};
+		case POST_FAILED:
+			return {
+				...state,
+				error: action.payload,
+			};
 	}
 };
