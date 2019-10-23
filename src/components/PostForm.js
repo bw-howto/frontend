2d84c41c18e79e159be2da9444069bef98c737e7
Chase Hooper <43388614+HoopCha@users.noreply.github.com>
2019-10-23 10:47:32 -0600

    most everything has styles, just need to add animations and fancy stuff

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,6 +1,93 @@
 import React from "react";
 import { connect } from "react-redux";
 import { addPost } from "../actions";
+import styled from "styled-components";
+
+const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	align-items:center;
+`;
+const FormBackground =styled.div`
+	background: #3a405a;
+	width: 30%;
+	height: 40px;
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+	padding: 10px;
+	border-radius: 6px;
+	margin: 30px;
+`;
+const PostTitle = styled.label`
+	font-size: 1.5rem;
+	text-align: center;
+	color: white;
+	font-weight: 600;
+	font-family: "Lato", sans-serif;
+	padding-right: 20px;
+	width: auto;
+`;
+const TitleInput = styled.input`
+height: 30px;
+width: 300px;
+padding: 12px 20px;
+box-sizing: border-box;
+border: 2px solid #ccc;
+border-radius: 4px;
+background-color: #f8f8f8;
+font-size: 16px;
+resize: none;
+`;
+
+const FormBackground2 =styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: center;
+	background: #3a405a;
+	width: 40%;
+	height: 240px;
+	padding: 10px;
+	border-radius: 6px;
+	margin-bottom: 30px;
+`;
+const PostTitle2 = styled.label`
+	font-size: 1.5rem;
+	text-align: center;
+	color: white;
+	font-weight: 600;
+	font-family: "Lato", sans-serif;
+	padding: 10px;
+`;
+const TitleInput2 = styled.textarea`
+width: 90%;
+height: 150px;
+padding: 12px 20px;
+box-sizing: border-box;
+border: 2px solid #ccc;
+border-radius: 4px;
+background-color: #f8f8f8;
+font-size: 16px;
+resize: none;
+`;
+
+const Button = styled.button`
+	font-family: "Lato", sans-serif;
+	font-size: 1rem;
+	font-weight: 800;
+	background-color: #de6e4b;
+	color: #3a405a;
+	text-align: center;
+	text-decoration: none;
+	margin: 10px;
+	padding: 25px 50px;
+	border-radius: 6px;
+	:hover {
+		color: white;
+	}
+`;
 
 const PostForm = props => {
 	const [newPost, setNewPost] = React.useState({
@@ -22,25 +109,29 @@ const PostForm = props => {
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<label htmlFor="postName">Post Title:</label>
-			<input
+		<Form onSubmit={handleSubmit}>
+			<FormBackground>
+			<PostTitle htmlFor="postName">Post Title:</PostTitle>
+			<TitleInput
 				type="text"
 				name="postName"
 				onChange={handleChange}
 				placeholder="Enter Title..."
 				value={newPost.postName}
 			/>
-			<label htmlFor="description">Post Body:</label>
-			<textarea
+			</FormBackground>
+			<FormBackground2>
+			<PostTitle2 htmlFor="description">Post Body:</PostTitle2>
+			<TitleInput2
 				onChange={handleChange}
 				col="5"
 				name="description"
 				value={newPost.description}
 				placeholder="Enter Text..."
 			/>
-			<button type="submit">Add Post</button>
-		</form>
+			</FormBackground2>
+			<Button type="submit">Add Post</Button>
+		</Form>
 	);
 };
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,46 @@
 import React, { useState } from "react";
 import { connect, useDispatch } from "react-redux";
 import { SEARCH_SUCCESS } from "../actions";
+import styled from "styled-components";
+
+const Form = styled.form`
+	display: flex;
+	justify-content: center;
+`;
+
+const FormBackground =styled.div`
+background: #de6e4b;
+	width: 380;
+	height: 45px;
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+	padding: 10px;
+	border-radius: 6px;
+`;
+const SearchWord = styled.label`
+	font-size: 1.5rem;
+	text-align: center;
+	color: white;
+	font-weight: 600;
+	font-family: "Lato", sans-serif;
+	padding-bottom: 5px;
+`;
+
+const Button = styled.button`
+	font-family: "Lato", sans-serif;
+	font-size: 1rem;
+	font-weight: 800;
+	background-color: #3a405a;
+	color: #de6e4b;
+	text-align: center;
+	text-decoration: none;
+	margin: 10px;
+	border-radius: 6px;
+	:hover {
+		color: white;
+	}
+`;
 
 const Search = props => {
 	const dispatch = useDispatch();
@@ -22,8 +62,9 @@ const Search = props => {
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<label htmlFor="searchTerm">Search:</label>
+		<Form onSubmit={handleSubmit}>
+			<FormBackground>
+			<SearchWord htmlFor="searchTerm">Search:</SearchWord>
 			<input
 				type="text"
 				onChange={e => handleChange(e)}
@@ -31,8 +72,9 @@ const Search = props => {
 				name="searchTerm"
 				placeholder="Search..."
 			/>
-			<button type="submit">Search</button>
-		</form>
+			<Button type="submit">Search</Button>
+			</FormBackground>
+		</Form>
 	);
 };
 
